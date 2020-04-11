e3f715edb5513ab0fd61f6f2b9eb18267b203648
ma.krylov <ma.krylov@tensor.ru>
2020-04-11 04:36:51 +0300

    Update docs

--- src/core/Ast.ts
+++ src/core/Ast.ts
@@ -7,27 +7,71 @@ import { Expression } from "../expression/Expression";
  */
 
 export enum Flags {
+   /**
+    * Flag for broken node that should be ignored.
+    */
    BROKEN = 1,
+   /**
+    * Flag for unpacked node that has been changed its view.
+    */
    UNPACKED = 2,
+   /**
+    * Flag for broken node that was repaired.
+    */
    REPAIRED = 4
 }
 
+/**
+ * Data representation type.
+ */
 declare type TData = ArrayNode | BooleanNode | FunctionNode | NumberNode | ObjectNode | StringNode | ValueNode;
+
+/**
+ * Text representation type.
+ */
 declare type TText = ProgramNode | TextNode;
+
+/**
+ * Content representation type.
+ */
 declare type TContent = PartialNode | IfNode | ElseNode | ForNode | ForeachNode | ElementNode | ProgramNode | TextNode;
 
-export interface ITextCollection {
-   [ name: string ]: TText;
+/**
+ * Interface for attributes collection.
+ */
+export interface IAttributesCollection {
+   /**
+    * @name {string} Attribute name.
+    * @returns {TText} Text or program node.
+    */
+   [name: string]: TText;
 }
 
+/**
+ * Interface for control or object properties.
+ */
 export interface IObjectProperties {
+   /**
+    * @name {string} Control or object property.
+    * @returns {TData | TContent} Content of object property.
+    */
    [name: string]: TData | TContent;
 }
 
+/**
+ * Represents interface for event handlers.
+ */
 export interface IEventsCollection {
-   [ name: string ]: ProgramNode;
+   /**
+    * @name {string} Event name.
+    * @returns {ProgramNode} Event handler.
+    */
+   [name: string]: ProgramNode;
 }
 
+/**
+ * Interface for visitor of abstract syntax nodes.
+ */
 export interface IAstVisitor {
    visitArray(node: ArrayNode, context: any): any;
    visitBoolean(node: BooleanNode, context: any): any;
@@ -51,30 +95,75 @@ export interface IAstVisitor {
    visitProgram(node: ProgramNode, context: any): any;
 }
 
+/**
+ * Declares abstract class for node of abstract syntax tree.
+ */
 export abstract class Ast {
+   /**
+    * Processing flags.
+    */
    flags: Flags;
 
+   /**
+    * Initialize new instance of abstract syntax node.
+    * @param flags Initialize flags.
+    */
    protected constructor(flags: Flags = 0) {
       this.flags = flags;
    }
 
+   /**
+    *
+    * @param visitor {IAstVisitor} Concrete visitor.
+    * @param context {*} Context.
+    */
    abstract accept(visitor: IAstVisitor, context: any): any;
 }
 
+/**
+ * Abstract class for node of abstract syntax tree that
+ * contains attributes and event handlers.
+ */
 export abstract class ActiveNode extends Ast {
-   attributes: ITextCollection;
+   /**
+    * Node attributes.
+    */
+   attributes: IAttributesCollection;
+   /**
+    * Node event handlers.
+    */
    events: IEventsCollection;
 
-   protected constructor(attributes: ITextCollection, events: IEventsCollection) {
+   /**
+    * Initialize new instance of abstract syntax node.
+    * @param attributes {IAttributesCollection} Node attributes.
+    * @param events {IEventsCollection} Node event handlers.
+    */
+   protected constructor(attributes: IAttributesCollection, events: IEventsCollection) {
       super();
       this.attributes = attributes;
       this.events = events;
    }
 }
 
+/**
+ * Represents node for ws:Array tag.
+ *
+ * ```
+ *    <ws:Array>
+ *       elements
+ *    </ws:Array>
+ * ```
+ */
 export class ArrayNode extends Ast {
+   /**
+    * Content elements.
+    */
    elements: TData[] = [];
 
+   /**
+    * Initialize new instance of abstract syntax node.
+    */
    constructor() {
       super();
    }
@@ -84,9 +173,22 @@ export class ArrayNode extends Ast {
    }
 }
 
+/**
+ * Represents node for ws:Boolean tag.
+ *
+ * ```
+ *    <ws:Boolean>
+ *       content
+ *    </ws:Boolean>
+ * ```
+ */
 export class BooleanNode extends Ast {
    content: ProgramNode;
 
+   /**
+    * Initialize new instance of abstract syntax node.
+    * @param content {ProgramNode} Content expression.
+    */
    constructor(content: ProgramNode) {
       super();
       this.content = content;
@@ -97,9 +199,22 @@ export class BooleanNode extends Ast {
    }
 }
 
+/**
+ * Represents node for ws:Function tag.
+ *
+ * ```
+ *    <ws:Function>
+ *       content
+ *    </ws:Function>
+ * ```
+ */
 export class FunctionNode extends Ast {
    content: ProgramNode;
 
+   /**
+    * Initialize new instance of abstract syntax node.
+    * @param content {ProgramNode} Content expression.
+    */
    constructor(content: ProgramNode) {
       super();
       this.content = content;
@@ -110,9 +225,22 @@ export class FunctionNode extends Ast {
    }
 }
 
+/**
+ * Represents node for ws:Number tag.
+ *
+ * ```
+ *    <ws:Number>
+ *       content
+ *    </ws:Number>
+ * ```
+ */
 export class NumberNode extends Ast {
    content: ProgramNode;
 
+   /**
+    * Initialize new instance of abstract syntax node.
+    * @param content {ProgramNode} Content expression.
+    */
    constructor(content: ProgramNode) {
       super();
       this.content = content;
@@ -123,9 +251,24 @@ export class NumberNode extends Ast {
    }
 }
 
+/**
+ * Represents node for ws:Object tag.
+ *
+ * ```
+ *    <ws:Object>
+ *       <ws:property>
+ *          content
+ *       </ws:property>
+ *    </ws:Object>
+ * ```
+ */
 export class ObjectNode extends Ast {
    properties: IObjectProperties;
 
+   /**
+    * Initialize new instance of abstract syntax node.
+    * @param properties {IObjectProperties} Object properties collection.
+    */
    constructor(properties: IObjectProperties) {
       super();
       this.properties = properties;
@@ -136,9 +279,22 @@ export class ObjectNode extends Ast {
    }
 }
 
+/**
+ * Represents node for ws:String tag.
+ *
+ * ```
+ *    <ws:String>
+ *       content
+ *    </ws:String>
+ * ```
+ */
 export class StringNode extends Ast {
    content: ProgramNode;
 
+   /**
+    * Initialize new instance of abstract syntax node.
+    * @param content {ProgramNode} Content expression.
+    */
    constructor(content: ProgramNode) {
       super();
       this.content = content;
@@ -149,9 +305,22 @@ export class StringNode extends Ast {
    }
 }
 
+/**
+ * Represents node for ws:Value tag.
+ *
+ * ```
+ *    <ws:Value>
+ *       content
+ *    </ws:Value>
+ * ```
+ */
 export class ValueNode extends Ast {
    content: ProgramNode;
 
+   /**
+    * Initialize new instance of abstract syntax node.
+    * @param content {ProgramNode} Content expression.
+    */
    constructor(content: ProgramNode) {
       super();
       this.content = content;
@@ -162,10 +331,23 @@ export class ValueNode extends Ast {
    }
 }
 
+/**
+ * Represents node for ws:template tag.
+ *
+ * ```
+ *    <ws:template name="template name">
+ *       content
+ *    </ws:template>
+ * ```
+ */
 export class TemplateNode extends Ast {
    name: string;
    content: TContent[] = [];
 
+   /**
+    * Initialize new instance of abstract syntax node.
+    * @param name {string} Template name.
+    */
    constructor(name: string) {
       super();
       this.name = name;
@@ -176,11 +358,27 @@ export class TemplateNode extends Ast {
    }
 }
 
+/**
+ * Represents node for ws:partial tag.
+ *
+ * ```
+ *    <ws:partial template="name or expression">
+ *       content
+ *    </ws:partial>
+ * ```
+ */
 export class PartialNode extends ActiveNode {
    name: ProgramNode | string;
    options: IObjectProperties;
 
-   constructor(name: ProgramNode | string, attributes: ITextCollection, options: IObjectProperties, events: IEventsCollection) {
+   /**
+    * Initialize new instance of abstract syntax node.
+    * @param name {string} Partial name.
+    * @param attributes {IAttributesCollection} Partial attributes.
+    * @param options {IObjectProperties} Partial properties.
+    * @param events {IEventsCollection} Partial event handlers.
+    */
+   constructor(name: ProgramNode | string, attributes: IAttributesCollection, options: IObjectProperties, events: IEventsCollection) {
       super(attributes, events);
       this.name = name;
       this.options = options;
@@ -191,11 +389,33 @@ export class PartialNode extends ActiveNode {
    }
 }
 
+/**
+ * Represents node for control tag.
+ *
+ * ```
+ *    <control attr:name="value" on:event="handler" option="value">
+ *       content
+ *    </control>
+ * ```
+ */
 export class ControlNode extends ActiveNode {
+   /**
+    * Control name.
+    */
    name: string;
+   /**
+    * Control properties.
+    */
    options: IObjectProperties;
 
-   constructor(name: string, attributes: ITextCollection, options: IObjectProperties, events: IEventsCollection) {
+   /**
+    * Initialize new instance of abstract syntax node.
+    * @param name {string} Control name.
+    * @param attributes {IAttributesCollection} Control attributes.
+    * @param options {IObjectProperties} Control properties.
+    * @param events {IEventsCollection} Control event handlers.
+    */
+   constructor(name: string, attributes: IAttributesCollection, options: IObjectProperties, events: IEventsCollection) {
       super(attributes, events);
       this.name = name;
       this.options = options;
@@ -206,11 +426,35 @@ export class ControlNode extends ActiveNode {
    }
 }
 
+/**
+ * Represents node for ws:if tag.
+ *
+ * ```
+ *    <ws:if data="expression">
+ *       content
+ *    </ws:if>
+ * ```
+ */
 export class IfNode extends Ast {
+   /**
+    * Test expression.
+    */
    test: ProgramNode;
+   /**
+    * Consequent nodes.
+    */
    consequent: TContent;
+   /**
+    * Alternate nodes.
+    */
    alternate: TContent | undefined;
 
+   /**
+    * Initialize new instance of abstract syntax node.
+    * @param test {ProgramNode} Test expression.
+    * @param consequent {TContent} Consequent nodes.
+    * @param alternate {TContent} Alternate nodes.
+    */
    constructor(test: ProgramNode, consequent: TContent, alternate?: TContent) {
       super();
       this.test = test;
@@ -223,11 +467,35 @@ export class IfNode extends Ast {
    }
 }
 
+/**
+ * Represents node for ws:else tag.
+ *
+ * ```
+ *    <ws:else data="expression">
+ *       content
+ *    </ws:else>
+ * ```
+ */
 export class ElseNode extends Ast {
+   /**
+    * Consequent nodes.
+    */
    consequent: TContent;
+   /**
+    * Test expression. If not empty then node equals to "else if".
+    */
    test: ProgramNode | undefined;
+   /**
+    * Alternate nodes that is not empty if and only if there is test expression.
+    */
    alternate: TContent | undefined;
 
+   /**
+    * Initialize new instance of abstract syntax node.
+    * @param consequent {TContent} Consequent nodes.
+    * @param test {ProgramNode} Test expression.
+    * @param alternate {TContent} Alternate nodes.
+    */
    constructor(consequent: TContent, test?: ProgramNode, alternate?: TContent) {
       super();
       this.consequent = consequent;
@@ -240,18 +508,44 @@ export class ElseNode extends Ast {
    }
 }
 
+/**
+ * Represents node for ws:for.
+ *
+ * ```
+ *    <ws:for data="init; test; update">
+ *       content
+ *    </ws:for>
+ * ```
+ */
 export class ForNode extends Ast {
+   /**
+    * Initialize expression.
+    */
    init: ProgramNode | undefined;
+   /**
+    * Test expression.
+    */
    test: ProgramNode | undefined;
+   /**
+    * Update expression.
+    */
    update: ProgramNode | undefined;
-   content: TContent[];
+   /**
+    * Content nodes.
+    */
+   content: TContent[] = [];
 
-   constructor(init: ProgramNode | undefined, test: ProgramNode | undefined, update: ProgramNode | undefined, content: TContent[]) {
+   /**
+    * Initialize new instance of abstract syntax node.
+    * @param init {ProgramNode | undefined} Initialize expression.
+    * @param test {ProgramNode | undefined} Test expression.
+    * @param update {ProgramNode | undefined} Update expression.
+    */
+   constructor(init: ProgramNode | undefined, test: ProgramNode | undefined, update: ProgramNode | undefined) {
       super();
       this.init = init;
       this.test = test;
       this.update = update;
-      this.content = content;
    }
 
    accept(visitor: IAstVisitor, context: any): any {
@@ -259,18 +553,44 @@ export class ForNode extends Ast {
    }
 }
 
+/**
+ * Represents node for ws:for.
+ *
+ * ```
+ *    <ws:for data="index, iterator in collection">
+ *       content
+ *    </ws:for>
+ * ```
+ */
 export class ForeachNode extends Ast {
+   /**
+    * Expression for iterator indexer.
+    */
    index: ProgramNode | undefined;
+   /**
+    * Iterator expression.
+    */
    iterator: ProgramNode;
+   /**
+    * Collection expression.
+    */
    collection: ProgramNode;
-   content: TContent[];
+   /**
+    * Content nodes.
+    */
+   content: TContent[] = [];
 
-   constructor(index: ProgramNode | undefined, iterator: ProgramNode, collection: ProgramNode, content: TContent[]) {
+   /**
+    * Initialize new instance of abstract syntax node.
+    * @param index {ProgramNode | undefined} Expression for iterator indexer.
+    * @param iterator {ProgramNode} Iterator expression.
+    * @param collection {ProgramNode} Collection expression.
+    */
+   constructor(index: ProgramNode | undefined, iterator: ProgramNode, collection: ProgramNode) {
       super();
       this.index = index;
       this.iterator = iterator;
       this.collection = collection;
-      this.content = content;
    }
 
    accept(visitor: IAstVisitor, context: any): any {
@@ -278,11 +598,32 @@ export class ForeachNode extends Ast {
    }
 }
 
+/**
+ * Represents node for element tag.
+ *
+ * ```
+ *    <tag attribute="value" on:event="handler">
+ *       content
+ *    </tag>
+ * ```
+ */
 export class ElementNode extends ActiveNode {
+   /**
+    * Element name.
+    */
    name: string;
+   /**
+    * Element children.
+    */
    content: TContent[] = [];
 
-   constructor(name: string, attributes: ITextCollection, events: IEventsCollection) {
+   /**
+    * Initialize new instance of abstract syntax node.
+    * @param name {string} Element name.
+    * @param attributes {IAttributesCollection} Element attributes collection.
+    * @param events {IEventsCollection} Element event handlers.
+    */
+   constructor(name: string, attributes: IAttributesCollection, events: IEventsCollection) {
       super(attributes, events);
       this.name = name;
    }
@@ -292,9 +633,19 @@ export class ElementNode extends ActiveNode {
    }
 }
 
+/**
+ * Represents node for text.
+ */
 export class TextNode extends Ast {
+   /**
+    * Text content.
+    */
    content: string;
 
+   /**
+    * Initialize new instance of abstract syntax node.
+    * @param content {string} Text content.
+    */
    constructor(content: string) {
       super();
       this.content = content;
@@ -305,9 +656,24 @@ export class TextNode extends Ast {
    }
 }
 
+/**
+ * Represents node for doctype declaration.
+ *
+ * @example
+ * ```
+ *    <!DOCTYPE content>
+ * ```
+ */
 export class DoctypeNode extends Ast {
+   /**
+    * Doctype content.
+    */
    content: string;
 
+   /**
+    * Initialize new instance of abstract syntax node.
+    * @param content {string} Doctype content.
+    */
    constructor(content: string) {
       super();
       this.content = content;
@@ -318,9 +684,23 @@ export class DoctypeNode extends Ast {
    }
 }
 
+/**
+ * Represents node for cdata declaration.
+ *
+ * ```
+ *    <![CDATA[ content ]]>
+ * ```
+ */
 export class CDataNode extends Ast {
+   /**
+    * Cdata section content.
+    */
    content: string;
 
+   /**
+    * Initialize new instance of abstract syntax node.
+    * @param content {string} Cdata section content.
+    */
    constructor(content: string) {
       super();
       this.content = content;
@@ -331,9 +711,23 @@ export class CDataNode extends Ast {
    }
 }
 
+/**
+ * Represents node for comment.
+ *
+ * ```
+ *    <!-- content -->
+ * ```
+ */
 export class CommentNode extends Ast {
+   /**
+    * Comment content.
+    */
    content: string;
 
+   /**
+    * Initialize new instance of abstract syntax node.
+    * @param content {string} Comment content.
+    */
    constructor(content: string) {
       super();
       this.content = content;
@@ -344,9 +738,23 @@ export class CommentNode extends Ast {
    }
 }
 
+/**
+ * Represents node for mustache expression.
+ *
+ * ```
+ *    {{ javascript expression }}
+ * ```
+ */
 export class ProgramNode extends Ast {
+   /**
+    * Program node expressions.
+    */
    expressions: Expression[];
 
+   /**
+    * Initialize new instance of abstract syntax node.
+    * @param expressions {Expression[]} Program node expressions.
+    */
    constructor(expressions: Expression[]) {
       super();
       this.expressions = expressions;
--- src/core/Transformer.ts
+++ src/core/Transformer.ts
@@ -7,45 +7,97 @@ import * as AstNodes from "./Ast";
  * @file src/core/Transformer.ts
  */
 
+/**
+ * Releases transformation from parse tree into abstract syntax tree.
+ */
 export class TransformVisitor implements RawNodes.IVisitor {
-   constructor() {
-      // TODO: Release finite state machine
-   }
 
+   /**
+    * Initialize new instance of transform visitor.
+    * @todo Release finite state machine.
+    */
+   constructor() { }
+
+   /**
+    * Visit all nodes in the given array and generate their ast representation.
+    * @param nodes {IVisitable} Collection of nodes.
+    * @param context {*} Context.
+    */
    visitAll(nodes: RawNodes.IVisitable[], context?: any): any {
       return nodes.map(node => node.accept(this, context));
    }
 
+   /**
+    * Visit attribute and generate its ast representation.
+    * @param node {Attribute} Attribute node.
+    * @param context {*} Context.
+    * @todo Release attribute transform
+    */
    visitAttribute(node: RawNodes.Attribute, context?: any): any {
-      // TODO: Release attribute transform
       return { };
    }
 
+   /**
+    * Visit cdata node and generate its ast representation.
+    * @param node {CData} CDATA section node.
+    * @param context {*} Context.
+    */
    visitCData(node: RawNodes.CData, context?: any): any {
       return new AstNodes.CDataNode(node.value);
    }
 
+   /**
+    * Visit comment node and generate its ast representation.
+    * @param node {Comment} Comment node.
+    * @param context {*} Context.
+    */
    visitComment(node: RawNodes.Comment, context?: any): any {
       return new AstNodes.CommentNode(node.value);
    }
 
+   /**
+    * Visit doctype node and generate its ast representation.
+    * @param node {Doctype} Doctype node.
+    * @param context {*} Context.
+    */
    visitDoctype(node: RawNodes.Doctype, context?: any): any {
       return new AstNodes.DoctypeNode(node.value);
    }
 
+   /**
+    * Visit tag node and generate its ast representation.
+    * @param node {Tag} Tag node.
+    * @param context {*} Context.
+    */
    visitTag(node: RawNodes.Tag, context?: any): any {
       return this.transform(node, context);
    }
 
+   /**
+    * Visit text node and generate its ast representation.
+    * @param node {Text} Text node.
+    * @param context {*} Context.
+    * @todo Release parsing text.
+    */
    visitText(node: RawNodes.Text, context?: any): any {
       return new AstNodes.TextNode(node.value);
    }
 
+   /**
+    * Transform tag node.
+    * @param node {Tag} Tag node.
+    * @param context {*} Context.
+    * @todo Release context transform using finite state machine.
+    */
    private transform(node: RawNodes.Tag, context?: any): any {
-      // TODO: Release context transform using finite state machine
       return this.transformElement(node, context);
    }
 
+   /**
+    * Transform element into abstract syntax element node.
+    * @param node {Text} Text node.
+    * @param context {*} Context.
+    */
    private transformElement(node: RawNodes.Tag, context?: any): any {
       const attributes = { };
       const events = { };
